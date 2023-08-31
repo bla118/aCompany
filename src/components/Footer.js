ce0f80898131d7ba310b7bafe051dbdce7090a30
bla118 <bla118@sfu.ca>
2023-08-30 17:55:00 -0700

    footer year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,11 @@
 import './Footer.css';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Footer = () => {
-  const [year, setYear] = useState('');
-
-  useEffect(() => {
-    const date = new Date();
-    const currentYear = date.getFullYear();
-    setYear(currentYear.toString());
-  }, []);
-  
   return (
     <footer className='footer'>
       <div className='footer-content'>
-        <p>&copy; {year} ICICOM Solutions. All rights reserved.</p>
+        <p>&copy; 2023 ICICOM Solutions. All rights reserved.</p>
         <div className='footer-links'>
           <a href='/'>Home</a>
           <a href='/services'>Services</a>
